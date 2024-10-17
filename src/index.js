ece99a4cab3c5f1f70e5b2dc1f621b1aa1b343b3
홍민정 <hongminjeong@hongminjeong-ui-MacBookAir.local>
2024-10-17 20:15:04 +0900

    🥳 컴퍼넌트완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+// scss 파일
 import './assets/scss/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
