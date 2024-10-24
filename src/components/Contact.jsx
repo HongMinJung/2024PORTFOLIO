ba5e4b0471ce628921f48ba329dc0e932000b174
홍민정 <hongminjeong@hongminjeong-ui-MacBookAir.local>
2024-10-25 01:16:46 +0900

    연락처 제작중

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,33 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <section id='contact'>
+      <div className='contact_inner'>
+        <h2 className='contact_title'>CONTACT</h2>
+        <div className='contact_form'>
+          <form action='#' className='form_box'>
+            <fieldset>
+              <legend className='form_title'>Please send the email</legend>
+              <div className='form'>
+                <label for='name' className='blind'></label>
+                <input type='text' d="name" placeholder="Name" required autocomplete="off" />
+                <label for='phone' className='blind'></label>
+                <input type='tel' d="phone" placeholder="Phone" required autocomplete="off" />
+                <label for='email' className='blind'></label>
+                <input type='email' d="email" placeholder="Email" required autocomplete="off" />
+              </div>
+
+              <div className='txt_form'>
+                <label for="message" class="blind"></label>
+                <textarea name="message" id="message" placeholder="Message" />
+              </div>
+            </fieldset>
+          </form>
+          <div class="send_btn">
+            <button type="submit">SEND MESSAGE</button>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
